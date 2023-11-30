e8b5b50effe879f7718ab43c2222b50c98aab98b
OmarZahrah <omar.zahrah01@gmail.com>
2023-11-30 08:30:59 +0200

    test

--- src/context/SignUpContext.jsx
+++ src/context/SignUpContext.jsx
@@ -3,6 +3,9 @@ import Step1 from "../features/signup/step1";
 import Step2 from "../features/signup/step2";
 import Step3 from "../features/signup/step3";
 import Step4 from "../features/signup/step4";
+import { useForm } from "react-hook-form";
+import customFetch from "../utils/customFetch";
+import { signup } from "../services/signup";
 
 const SignUpContext = createContext();
 
@@ -15,8 +18,50 @@ const steps = [
 
 const SignUpProvider = ({ children }) => {
   const [currentStep, setCurrentStep] = useState(1);
+  const { register, handleSubmit } = useForm();
+  const [profilePhoto, setProfilePhoto] = useState("");
+  const [coverPhoto, setCoverPhoto] = useState([]);
+  const [albumImages, setAlbumImages] = useState([]);
+  const [profilePhotoFile, setProfilePhotoFile] = useState("");
+  const [coverPhotoFile, setCoverPhotoFile] = useState("");
+  const [albumPhotosFile, setAlbumPhotosFile] = useState("");
+
+  const onSubmit = async (formData) => {
+    // const profile = Array.from(formData.profile);
+    console.log(profilePhotoFile);
+    // console.log(profile);
+    // console.log(profilePic);
+    const finalData = {
+      ...formData,
+      role: "provider",
+      photo: profilePhotoFile,
+      coverPhoto: coverPhotoFile,
+      // photoAlbum: [...Object.values(albumPhotosFile)],
+    };
+    console.log(finalData);
+    console.log(signup(finalData));
+  };
+
   return (
-    <SignUpContext.Provider value={{ steps, currentStep, setCurrentStep }}>
+    <SignUpContext.Provider
+      value={{
+        steps,
+        currentStep,
+        setCurrentStep,
+        register,
+        handleSubmit,
+        onSubmit,
+        profilePhoto,
+        setProfilePhoto,
+        coverPhoto,
+        setCoverPhoto,
+        albumImages,
+        setAlbumImages,
+        setProfilePhotoFile,
+        setCoverPhotoFile,
+        setAlbumPhotosFile,
+      }}
+    >
       {children}
     </SignUpContext.Provider>
   );
