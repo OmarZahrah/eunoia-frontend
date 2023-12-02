e45867c4938839957407553af0905c5835f06a1f
OmarZahrah <omar.zahrah01@gmail.com>
2023-12-02 18:29:44 +0200

    edit welcome page

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -4,37 +4,37 @@ import Button from "../components/Button";
 import image1 from "../../public/images/unsplash-ebv-cs-rypmx-m.png";
 import image2 from "../../public/images/unsplash-fn-0-xxl-9-szc-u.png";
 import image3 from "../../public/images/unsplash-azod-v-2-d-0-g-lc.png";
-import group from "../../public/group-26.svg";
 import { Link } from "react-router-dom";
 const Welcome = () => {
   return (
     <Wrapper>
+      {/* <div className="container"> */}
+      <nav>
+        <img className="Hlogo" src={logo} />
+        <div className="buttons">
+          <Link to="/login">
+            <Button
+              className="logbutton"
+              color="pink"
+              background="transparent"
+              size="small"
+            >
+              Login
+            </Button>
+          </Link>
+          <Link to="/signup/create">
+            <Button
+              className="regbutton"
+              color="white"
+              background="pink"
+              size="small"
+            >
+              Register
+            </Button>
+          </Link>
+        </div>
+      </nav>
       <div className="container">
-        <nav>
-          <img className="Hlogo" src={logo} />
-          <div className="buttons">
-            <Link to="/login">
-              <Button
-                className="logbutton"
-                color="pink"
-                background="transparent"
-                size="small"
-              >
-                Login
-              </Button>
-            </Link>
-            <Link to="/signup/create">
-              <Button
-                className="regbutton"
-                color="white"
-                background="pink"
-                size="small"
-              >
-                Register
-              </Button>
-            </Link>
-          </div>
-        </nav>
         <div className="images">
           <img src={image1} />
           <img src={image2} />
@@ -49,6 +49,7 @@ const Welcome = () => {
             ultimate guide to seamless and unforgettable gatherings.
           </p>
         </div>
+        {/* </div> */}
       </div>
     </Wrapper>
   );
@@ -56,11 +57,27 @@ const Welcome = () => {
 export default Welcome;
 
 const Wrapper = styled.div`
+  height: 100vh;
+  background: linear-gradient(to top, #fef9f0 55%, #fff 45%);
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  align-items: center;
   nav {
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 1.1rem 8rem;
+    /* padding: 1.1rem 8rem; */
+    width: 80%;
+    padding-top: 1rem;
+    /* margin: 0 auto; */
+  }
+  .container {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    /* justify-content: center; */
+    /* padding-top: 1rem; */
   }
 
   .Hlogo {
@@ -69,12 +86,7 @@ const Wrapper = styled.div`
     flex-shrink: 0;
   }
 
-  .buttons {
-    margin-right: 1.2rem;
-  }
-
   .logbutton {
-    margin-right: 0rem;
     font-size: 1.5rem;
   }
 
@@ -86,45 +98,42 @@ const Wrapper = styled.div`
     height: 3.125rem;
     flex-shrink: 0;
   }
-
-  .container {
-    background: linear-gradient(to top, #fef9f0 55%, #fff 45%);
-    height: 100vh;
-    margin-top: 1.8rem;
-  }
   .images {
     display: flex;
-    justify-content: space-between;
-    margin: 1.125rem 7.5rem 1.25rem;
+    width: 80%;
+    margin: 0 auto;
+    /* padding: 20px; */
+    gap: 1rem;
+    justify-content: center;
   }
   .images img {
-    width: 24rem;
-    height: auto;
+    width: 32%;
     padding: 17px;
   }
 
   .h {
     color: var(--text, rgba(0, 0, 0, 0.6));
-    text-align: center;
-    font-size: 2.8125rem;
+    font-size: 2.5rem;
     font-family: "literata", serif;
-    font-style: "Lucida Console", "Courier New", monospace;
-    font-weight: 400;
-    font-optical-sizing: auto;
+    font-weight: 510;
     font-style: italic;
-    line-height: normal;
     letter-spacing: 0.0625rem;
     width: 50%;
-    margin: 0 auto;
   }
 
   p {
     color: var(--text, rgba(0, 0, 0, 0.6));
     text-align: center;
-    font-size: 1.36rem;
+    font-size: 1.2rem;
     font-weight: 300;
     letter-spacing: 0.0625rem;
-    width: 46.875rem;
-    margin: 9px 0 0 21.25rem;
+    width: 55%;
+    /* padding-bottom: 1rem; */
+  }
+  .text {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 5px;
   }
 `;
