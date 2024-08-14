5af734de624f39fe34863a4a97833d6686cc24df
OmarZahrah <omar.zahrah01@gmail.com>
2024-08-14 12:14:47 +0300

    graduation version

--- src/utils/customFetch.js
+++ src/utils/customFetch.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const customFetch = axios.create({
   baseURL: "/api/v1",
-  // baseURL: "http://3.129.5.59:3000/api/v1/",
+  // baseURL: import.meta.env.VITE_API,
 });
 
 export default customFetch;
